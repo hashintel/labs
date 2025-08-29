cef7820136fc8def5342ec1b033bce1e4c8adeb8
Ciaran Morinan <c@hash.ai>
2025-08-29 10:42:21 +0100

    merge conflicts

--- pocs/petrinaut-automerge/vite.config.ts
+++ pocs/petrinaut-automerge/vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
-<<<<<<< HEAD
 	build: {
 		target: "esnext",
 	},
@@ -15,19 +14,4 @@ export default defineConfig({
 		format: "es",
 		plugins: () => [wasm()],
 	},
-=======
-  // customize this to your repo name for github pages deploy
-  // base: "/automerge-repo-quickstart/",
-
-  build: {
-    target: "esnext",
-  },
-
-  plugins: [wasm(), react()],
-
-  worker: {
-    format: "es",
-    plugins: () => [wasm()],
-  },
->>>>>>> main
 });
