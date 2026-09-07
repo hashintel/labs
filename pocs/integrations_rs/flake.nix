a5e84f5e365fb4fbdbf1a02e4d9dee3037980767
Ahmad Sattar <thehabbos007@gmail.com>
2026-09-07 09:51:56 +0200

    Update flake, move to top level

--- pocs/integrations_rs/flake.nix
+++ pocs/integrations_rs/flake.nix
@@ -1,29 +1,57 @@
 {
   description = "integrations_rs: Rust port of the integrations engine";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
-    in
-    {
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              rustc
-              cargo
-              cargo-nextest
-              clippy
-              rustfmt
-              # duckdb bundled build
-              gcc
-              cmake
-              pkg-config
-            ];
-          };
-        });
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
     };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        toolchain = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+          extensions = [
+            "rust-src"
+          ];
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rust-analyzer-unwrapped
+            cargo-flamegraph
+            cargo-machete
+            cargo-nextest
+            gcc
+            cmake
+            pkg-config
+            tlaplus
+          ];
+          buildInputs = [
+            toolchain
+          ];
+          RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+        };
+      }
+    );
 }
